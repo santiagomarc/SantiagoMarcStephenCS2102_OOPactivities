3a10f081586b7264fa3b6450c718051bc180613e
Marc Santiago <163503280+santiagomarc@users.noreply.github.com>
2024-11-22 14:08:05 +0800

    artists

---
+++ Laboratory Activities/Laboratory Activity 3 - 22-11-2024/Artists/ArtistDemo.java
@@ -0,0 +1,22 @@
+public class ArtistDemo {
+    public static void main(String[] args) {
+        Artist artist = new Artist("Charlie Puth", "American", 32, "Music");
+        artist.displayInfo();
+        System.out.println();
+
+        Singer singer = new Singer("Ariana Grande", "American", 31, "Music", Genre.POP);
+        singer.displayInfo();
+        System.out.println();
+
+        Painter painter = new Painter("Fernando Amorsolo", "Filipino", 79, "Art", Medium.OIL);
+        painter.displayInfo();
+        System.out.println();
+
+        Writer writer = new Writer("J.K. Rowling", "British", 59, "Literature", WritingStyle.FICTION);
+        writer.displayInfo();
+        System.out.println();
+
+        Dancer dancer = new Dancer("Hoshi", "Korean", 28, "Dance", DanceStyle.HIPHOP);
+        dancer.displayInfo();
+    }
+}
